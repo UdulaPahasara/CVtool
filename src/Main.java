c933d92b46ba8b3f0717f36df04b540e06a0d4a7
UdulaPahasara <udulapahasara2@gmail.com>
2026-02-17 09:26:59 -0800

    init main frame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        JFrame frame = new JFrame("Online Job Portal Ranking System");
+        frame.setSize(600,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        JLabel label = new JLabel("Welcome to Resume Ranking System", SwingConstants.CENTER);
+        frame.add(label);
+
+        frame.setVisible(true);
+
+
     }
 }
