ab2884a1cca2bc462d29b624e53a2a313f194a37
UdulaPahasara <udulapahasara2@gmail.com>
2026-02-22 05:36:35 -0800

    fix the experience logic

--- src/jobportal/util/PDFParserUtil.java
+++ src/jobportal/util/PDFParserUtil.java
@@ -29,9 +29,7 @@ public class PDFParserUtil {
         if (education == null || education.isEmpty())
             education = "Unknown";
 
-        int expScore = getExperienceScore(experienceStr);
-
-        return new Candidate(candidateId, name, skills, expScore, education, jobRole);
+        return new Candidate(candidateId, name, skills, experienceStr, education, jobRole);
     }
 
     private static String extractText(String filePath) throws IOException {
@@ -55,17 +53,4 @@ public class PDFParserUtil {
         return "";
     }
 
-    private static int getExperienceScore(String exp) {
-        exp = exp.toLowerCase();
-        if (exp.contains("1-2 years"))
-            return 20;
-        if (exp.contains("2-4 years"))
-            return 30;
-        if (exp.contains("5-9 years"))
-            return 60;
-        if (exp.contains("10+ years") || exp.contains("10 years") || exp.contains("11 years")
-                || exp.contains("12 years"))
-            return 80;
-        return 0;
-    }
 }
